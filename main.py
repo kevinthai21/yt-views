8902d9d17b855d04abb1f3305663e671b708ca01
Kevin <kevin.thai21@gmail.com>
2021-05-13 13:00:49 -0700

    can update video details

--- main.py
+++ main.py
@@ -7,50 +7,74 @@
 from datetime import date, datetime
 
 import os
+from dotenv import load_dotenv
 from youtube import YouTube
 
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
+load_dotenv('#.env')
 
 # Getting environment variables
 key = os.getenv('KEY')
 idVideo = os.getenv('ID_VIDEO')
 idChannel = os.getenv('ID_CHANNEL')
-#videoID = os.getenv('VIDEO_ID')
+client = os.getenv('CLIENT')
+category = os.getenv('CATEGORY')
 
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+scopes = ["https://www.googleapis.com/auth/youtubepartner",
+            "https://www.googleapis.com/auth/youtube",
+            "https://www.googleapis.com/auth/youtube.force-ssl"]
 
 video = YouTube(idVideo)
-
 def main():
 
     api_service = 'youtube'
     api_version = 'v3'
-
-    # TODO: Getting credentials does not work at the moment.
+    client_secrets_file = "client_secret_514071497858-gg76s3kneij8de17lon4sfbvc0amnlhp.apps.googleusercontent.com.json"
 
     # Getting credentials and create API client
-    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-    #     client_secrets_file, scopes)
-    # credentials = flow.run_console()
-    # youtube = googleapiclient.discovery.build(
-    #     api_service, api_version, credentials=credentials)
-    
-    # request = youtube.channels().list (
-    #     part = 'contentDetails',
-    #     id = video.getID()
-    # )
-    # response = request.execute()
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        api_service, api_version, credentials=credentials)
 
-    updateTitle()
-    updateDesciption()
 
+    request_read = youtube.videos().list(
+        part = "statistics",
+        id = video.id
+    )
+
+    request_update = youtube.videos().update(
+        part="snippet, contentDetails",
+        body={
+            "id": video.id,
+        
+            "snippet": {
+                "description": updateDesciption(),
+                "categoryId": category,
+                "title": updateTitle()
+            }
+        }
+    )
+
+    #TODO: Get the view count.
+
+    response = request_read.execute()
+    # print(response.items)
+    # print(response.items[0])
+    # print("Response: " , response.items[0].statistics.viewCount)
+
+    response = request_update.execute()
+
+    # updateVideo()
 
 def updateTitle():
     new_title = "This video has " + str(video.getViews()) + " views!"
     video.setTitle(new_title)
+    return(video.title)
 
 def updateDesciption():
     time = datetime.now()
@@ -59,8 +83,13 @@ def updateDesciption():
     des_ref = 'This is a personal project of mine that is inspired by Tom Scott!\n'
     des_ref_link = 'You can check out his video on this here: https://www.youtube.com/watch?v=BxV14h0kFs0\n'
     des_time = 'Last updated at ' + time_string
-    new_description = des_ref + des_ref_link + des_time
+    new_description = des_ref + "\n" + des_ref_link + "\n" + des_time
 
     video.setDescription(new_description)
+    return(video.description)
+
+def updateVideo():
+    updateTitle()
+    updateDesciption()
 
 main()
